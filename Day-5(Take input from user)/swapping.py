d63e779cb27a07e7a032a03c85cf2fbe592efe13
yogitashete2412 <yogitashete24@gmail.com>
2023-06-12 18:55:57 +0530

    doc : swapping examples added

--- Day-5(Take input from user)/swapping.py
+++ Day-5(Take input from user)/swapping.py
@@ -1,8 +1,12 @@
 a = 10
 b = 20
+
+print("Before swapping value of a",a)
+print("Before swapping value of b",b)
+
 temp = b
 b = a
 a = temp
 
-print(a)
-print(b)
+print("After swapping value of a",a)
+print("After swapping value of b",b)
