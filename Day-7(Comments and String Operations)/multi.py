dd38f71f896aa20da060f7a5ed863937f7943a10
yogitashete2412 <yogitashete24@gmail.com>
2023-06-15 08:57:07 +0530

    comments ex added

---
+++ Day-7(Comments and String Operations)/multi.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+"""
+This is a Hello World program.
+This is sample code.
+We used multiple line commet"""
---
+++ Day-7(Comments and String Operations)/single.py
@@ -0,0 +1,2 @@
+print("Hello World")
+# This is Hello World program.
