22599b4411133de138b0c8e686e7f3e02eeaf2f6
yogitashete2412 <yogitashete24@gmail.com>
2023-06-17 18:28:40 +0530

    tuple properties added

--- Day-9(Tuples)/tuple.py
+++ Day-9(Tuples)/tuple.py
@@ -9,4 +9,18 @@
 myBasket = ("Apple", "Banana", "Graphs", "Orange")
 print(myBasket)
 
-# output : ("Apple", "Banana", "Graphs", "Orange")
+# output : ('Apple', 'Banana', 'Graphs', 'Orange')
+
+# Manipulation of tuple : 
+
+# we created variable myBasket and store the values and created another variable for converting tuples into list 
+
+# tuples==>list==>update==>tuple
+
+myBasket = ("Apple", "Banana", "Graphs", "Orange")
+listMyBasket = list(myBasket)
+listMyBasket[0] = "Cherry"
+myBasket = tuple(listMyBasket)
+print(myBasket)
+
+# output : ('Cherry', 'Banana', 'Graphs', 'Orange', 'Apple)
