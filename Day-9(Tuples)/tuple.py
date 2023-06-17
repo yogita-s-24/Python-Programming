4420fffb96fcb240356dae82962c12bd23858178
yogitashete2412 <yogitashete24@gmail.com>
2023-06-17 19:00:13 +0530

    count method added

--- Day-9(Tuples)/tuple.py
+++ Day-9(Tuples)/tuple.py
@@ -6,16 +6,20 @@
 # 2. Unchangable 
 # 3. Allow duplicates
 
+# Example 1 :
+
 myBasket = ("Apple", "Banana", "Graphs", "Orange")
 print(myBasket)
 
 # output : ('Apple', 'Banana', 'Graphs', 'Orange')
 
-# Manipulation of tuple : 
+# * Manipulation of tuple : 
 
 # we created variable myBasket and store the values and created another variable for converting tuples into list 
 
 # tuples==>list==>update==>tuple
+
+# Example 2 :
 
 myBasket = ("Apple", "Banana", "Graphs", "Orange")
 listMyBasket = list(myBasket)
@@ -24,3 +28,38 @@ myBasket = tuple(listMyBasket)
 print(myBasket)
 
 # output : ('Cherry', 'Banana', 'Graphs', 'Orange', 'Apple)
+
+# Example 3 :
+
+pystudent = ("Saurabh", "Harshu", "Yogii")
+print(pystudent.index("Saurabh"))
+
+#Output : 0
+
+# Example 4 :
+
+pystudent = ("Saurabh", "Harshu", "Yogii")
+print(pystudent.index("Yogii"))
+
+#Output : 2
+
+
+# * Count Method
+
+# Example 5:
+
+pystudent = ("Saurabh", "Harshu","Harshu", "Yogii", "Yogii")
+print(pystudent.index("Yogii"))
+
+#Output : 2
+
+# Example 6:
+
+pystudent = ("Saurabh", "Harshu","Harshu", "Yogii", "Yogii")
+print(pystudent.index("Saurabh"))
+
+#Output : 1
+
+
+# * Concatenation of String
+
