90e04443027381352695e8cfdaeb1e819bdb60a6
yogitashete2412 <yogitashete24@gmail.com>
2023-06-18 22:52:42 +0530

    slicing ex added

--- Day-8(List)/list.py
+++ Day-8(List)/list.py
@@ -3,12 +3,23 @@
 #List
 students = ['Aachal','Aarav','Avinash','Dolly','Deva']
 print(students)
+#Output :['Aachal', 'Aarav', 'Avinash', 'Dolly', 'Deva']
+
 
 #Slicing on List
 #Example 1
 students = ['Aachal','Aarav','Avinash','Dolly','Deva']
 print(students[0:4])
+#Output :['Aarav', 'Avinash', 'Dolly']
+
 
 #Example 2
 students = ['Aachal','Aarav','Avinash','Dolly','Deva']
 print(students[4:])
+#Output :['Deva']
+
+
+#Example 3
+students = ['Aachal','Aarav','Avinash','Dolly','Deva']
+print(students[-2])
+#Output : Dolly  (It is delete from last)
