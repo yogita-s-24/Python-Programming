e251f2a28fe58bd92f9c7e41cd9dc9af51fb30df
yogitashete2412 <yogitashete24@gmail.com>
2023-06-18 22:47:02 +0530

    slicing ex added

--- Day-8(List)/list.py
+++ Day-8(List)/list.py
@@ -1,4 +1,9 @@
 # list are store value in [] brakets
 
+#List
 students = ['Aachal','Aarav','Avinash','Dolly','Deva']
 print(students)
+
+#Slicing on List
+students = ['Aachal','Aarav','Avinash','Dolly','Deva']
+print(students[0:4])
