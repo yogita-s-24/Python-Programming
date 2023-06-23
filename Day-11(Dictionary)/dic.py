32efc382a42a3df17a62e80607169c854b8d5f8f
yogitashete2412 <yogitashete24@gmail.com>
2023-06-23 16:03:53 +0530

    dictionary examples added

---
+++ Day-11(Dictionary)/dic.py
@@ -0,0 +1,83 @@
+#Day-11
+# Dictionaries are used to store data values in Key:value pairs
+# we used {} for dictionary 
+
+# Syntax :
+
+# dictionary_name = { key1 : value1, key2 : value2, ......}
+
+#Example :
+
+
+phoneBook = {
+    "Dolly" : "1234567890",
+    "Anu" : "9876543210",
+    "Avinash" : "21323422222",
+    "Pooja" : "878975457"
+}
+# print(phoneBook)
+#print(phoneBook["Pooja"])
+print("Anu =",phoneBook["Anu"])
+
+# Output : Anu = 9876543210
+
+#Example 2:
+
+phoneBook = {
+    "Dolly" : "1234567890",
+    "Anu" : "9876543210",
+    "Avinash" : "21323422222",
+    "Pooja" : "878975457"
+}
+print(phoneBook.get("Avinash"))
+
+#get() method is used to read the value
+
+# Output : 21323422222
+
+#Example 3:
+
+phoneBook = {
+    "Dolly" : "1234567890",
+    "Anu" : "9876543210",
+    "Avinash" : "21323422222",
+    "Pooja" : "878975457"
+}
+
+name = input("Enter name to find mob no :")
+mobile = phoneBook.get(name)
+print("Mobile Number :",mobile)
+
+# Output : 
+# Enter name to find mob no :Pooja
+# Mobile Number : 878975457
+
+
+#Example : 5
+
+courses ={
+    "C" :"499",
+    "C++" :"499",
+    "Python" :"499",
+    "DSA" :"999",
+    "ICP" :"999",
+    "ICGP" :"10,000"
+}
+print(courses.keys())
+
+#Output : dict_keys(['C', 'C++', 'Python', 'DSA', 'ICP', 'ICGP'])
+
+
+#Example : 6
+
+courses ={
+    "C" :"499",
+    "C++" :"499",
+    "Python" :"499",
+    "DSA" :"999",
+    "ICP" :"999",
+    "ICGP" :"10,000"
+}
+print(courses.values())
+
+#Output : dict_values(['499', '499', '499', '999', '999', '10,000'])
