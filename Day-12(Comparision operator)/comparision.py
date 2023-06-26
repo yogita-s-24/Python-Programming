449a24f32e71c93e9545a61925f90bb3a18bce19
yogitashete2412 <yogitashete24@gmail.com>
2023-06-26 08:18:10 +0530

    equal to ex added

---
+++ Day-12(Comparision operator)/comparision.py
@@ -0,0 +1,14 @@
+#Day-12
+#Comparision operator / Relational operator
+# 1. equal to (==)
+# 2. less than (<)
+# 3. greater than(>)
+# 4. less than or equal to (<=)
+# 5. greater than or equal to (>=)
+
+# 1. equal to (==)
+#Example 1
+a = 10
+b = 10
+result = a==b
+print(result)
