ea9f9d07bc1ba4d0bc6d75d61b9742f350ae763b
yogitashete2412 <yogitashete24@gmail.com>
2023-06-27 07:43:06 +0530

    doc: discount que convert in simplely added

--- Day-13(Questions)/que.py
+++ Day-13(Questions)/que.py
@@ -21,3 +21,28 @@ else :
 # Actual Bill 48
 # Discount : 2.4
 # Payable Ammount : 45.6
+
+# Example in short
+
+rate, quantity = input("Enter rate and quantity").split()
+bill = int(rate) * int(quantity)
+disc_per = 0
+
+if bill>500:
+  disc_per = 20
+else :
+  disc_per = 5
+
+discount = (bill/100)*disc_per
+paybleAmount = bill - discount
+print("##### {} OFF #####".format(disc_per))
+print("Actual Bill",bill)
+print("Discount",discount)
+print("Payable Amount",paybleAmount)
+
+#Output:
+# Enter rate and quantity12 20
+# ##### 5 OFF #####
+# Actual Bill 240
+# Discount 12.0
+# Payable Amount 228.0
