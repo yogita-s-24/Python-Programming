92f4d03393c54a075d6517858161b6cf8f5ec9ef
yogitashete2412 <yogitashete24@gmail.com>
2023-06-28 22:17:12 +0530

    ex added

--- Day-14(if-else-ladder)/index.py
+++ Day-14(if-else-ladder)/index.py
@@ -7,3 +7,31 @@ result = hsc>60 and jee>120
 print(result)
 #Output: True
 
+
+## 2.  OR
+## 2. OR
+hsc = 80
+jee = 140
+result = hsc>60 or jee>120
+print(result)
+#Output: True
+
+
+## 3. if-else-ladder
+num = int(input("Enter your name = "))
+if num==1:
+  print("You select option 1")
+elif num==2:
+   print("You select option 2")
+elif num==3:
+   print("You select option 3")
+elif num==4:
+   print("You select option 4")
+elif num==5:
+   print("You select option 5")
+else:
+   print("You select wrong option")
+   
+#Output: 
+# Enter your name = 5
+# You select option 5
