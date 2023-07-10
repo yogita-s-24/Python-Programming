dbc8880cc31985399c956768fa290e8dd57c48b7
yogitashete2412 <yogitashete24@gmail.com>
2023-07-10 22:40:46 +0530

    eg added Arguments but No Return

--- Day-16/fun.py
+++ Day-16/fun.py
@@ -18,3 +18,27 @@ def cal_si():
   print(si)
 cal_si()
 
+#Output :
+# Enter Principle : 80
+# Enter Rate : 500
+# Enter time : 90
+# 36000.0
+
+#2. Arguments but No Return
+
+def cal_si(p,r,t):
+  si = p*r*t/100
+  print(si)
+
+Amount = int(input("Enter Amount : "))
+Rate = int(input("Enter Rate : "))
+Time = int(input("Enter Time : "))
+cal_si(Amount, Rate, Time)
+
+# Output :
+# Enter Amount : 2000
+# Enter Rate : 70
+# Enter Time : 120
+# 168000.0
+
+
