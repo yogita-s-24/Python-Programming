ef310b9ab318766af669a4ce366e1dd434d224ed
yogitashete2412 <yogitashete24@gmail.com>
2023-07-10 22:38:41 +0530

    eg added No Arguments And No Return

--- Day-16/fun.py
+++ Day-16/fun.py
@@ -4,3 +4,17 @@
 #2. Arguments but No Return
 #3. No Arguments but Return
 #4. Arguments And Return
+
+
+# HW-9
+
+#1.No Arguments And No Return
+
+def cal_si():
+  p = int(input("Enter Principle : "))
+  r = int(input("Enter Rate : "))
+  t = int(input("Enter time : "))
+  si = p*r*t/100
+  print(si)
+cal_si()
+
